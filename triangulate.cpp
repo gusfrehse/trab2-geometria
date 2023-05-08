a69f3dd941fa9d0281597c1406ddb5b2038a1b14
Gustavo S Frehse <gusfrehse@gmail.com>
2023-05-08 09:32:32 -0300

    Update triangulate

--- triangulate.cpp
+++ triangulate.cpp
@@ -2,6 +2,26 @@
 
 #include "util.hpp"
 
+struct triangle {
+    std::array<int, 3> vertices;
+    std::array<int, 3> neighbours;
+}
+
 int main() {
+    int n;
+    std::vector<vec2<float>> polygon;
+
+    std::cin >> n;
+
+    polygon.resize(n);
+
+    for (int i = 0; i < n; i++) {
+        float x, y;
+
+        std::cin >> x >> y;
+
+        polygon[i] = vec2(x, y);
+    }
+
     return 0;
 }
