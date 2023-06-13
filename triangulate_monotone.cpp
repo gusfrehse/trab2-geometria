a6cef135eef94937548f37c60e81ca4cd378d077
Gustavo S Frehse <gusfrehse@gmail.com>
2023-06-13 15:03:57 -0300

    asdfasdf

--- triangulate_monotone.cpp
+++ triangulate_monotone.cpp
@@ -1,5 +1,8 @@
+#include <vector>
+#include <stack>
 
 #include "dcel.hpp"
+
 
 
 HalfEdgeId topMostVertex(DCEL &dcel, FaceId face)
@@ -24,12 +27,78 @@ HalfEdgeId topMostVertex(DCEL &dcel, FaceId face)
     return he;
 }
 
-void triangulateMonotoneFace(DCEL &dcel, FaceId face)
+std::vector<std::pair<HalfEdgeId, int>> mergeVertices(DCEL &dcel, FaceId face)
 {
+    std::vector<std::pair<HalfEdgeId, int>> u;
     HalfEdgeId he = topMostVertex(dcel, face);
-    HalfEdgeId eh = dcel.next(dcel.twin(he));
+    HalfEdgeId eh = he;
+    
+    // Merge vertices in decreasing y
+    do
+    {
+        if (dcel.coords(dcel.origin(he)).y >= dcel.coords(dcel.origin(eh)).y)
+        {
+            u.push_back({he, 0});
+            he = dcel.next(he);
+        }
+        else
+        {
+            u.push_back({eh, 1});
+            eh = dcel.prev(eh);            
+        }
+    }
+    while (he != eh);
+
+    return u;
 }
 
+void triangulateMonotoneFace(DCEL &dcel, FaceId face)
+{
+    std::vector<std::pair<HalfEdgeId, int>> u = mergeVertices(dcel, face);
+    
+    std::stack<std::pair<HalfEdgeId, int>> s;
+    
+    s.push(u[0]);
+    s.push(u[1]);
+    
+    for (int j = 2, n = u.size() - 1; j < n; j++)
+    {
+        if (u[j].second != s.top().second)
+        {
+            // Opposite sides
+
+            while (s.size() > 1)
+            {
+                HalfEdgeId he = s.top().first;
+                s.pop();
+                
+                dcel.connect(u[j].first, he);
+                s.push(u[j - 1]);
+                s.push(u[j]);
+            }
+        }
+        else
+        {
+            // Same side
+            s.pop();
+
+            HalfEdgeId v = s.top().first;
+
+            vec2 a = dcel.coords(u[j].first) - dcel.coords(v);
+            vec2 e1 = dcel.coords(dcel.prev(v));
+            vec2 e2 = dcel.coords(dcel.next(v));
+            
+            double cross1 = det(a, e1);
+            double cross2 = det(e2, a);
+            
+            if (cross1 * cross2 > 0)
+            {
+                // Same sign
+
+            }
+        }
+    }
+}
 
 void triangulateMonotone(DCEL &dcel)
 {
