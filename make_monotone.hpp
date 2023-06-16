9f6b118dcd0bc6dbc398a9dbfe5055174ad01b47
Gustavo S Frehse <gusfrehse@gmail.com>
2023-06-16 09:52:54 -0300

    asdf

--- make_monotone.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "dcel.hpp"
-#include "util.hpp"
-
-#include <vector>
-
-DCEL makeMonotone(std::vector<vec2> vertices);
--- triangulate_monotone.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "dcel.hpp"
-
-void triangulateMonotone(DCEL &dcel);
